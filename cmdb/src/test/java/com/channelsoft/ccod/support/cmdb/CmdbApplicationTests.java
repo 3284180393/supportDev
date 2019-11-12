2857fcafc1c0344080e0bedc76039135da2c6676
lanhb <lanhb@channelsoft.com>
2019-11-12 18:26:39 +0800

    develop

---
+++ cmdb/src/test/java/com/channelsoft/ccod/support/cmdb/CmdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.channelsoft.ccod.support.cmdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmdbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
